9d5975ea56088824d3c89a90b378473690371cdd
cyril <cyril.cermak@profinit.eu>
2016-12-28 21:13:41 +0100

    Add reading data from table

--- UCI_Cyril_Cermak.sql
+++ UCI_Cyril_Cermak.sql
@@ -1,26 +1,33 @@
 set serveroutput on;
 
+create Table Customers (
+	firstName varchar2(15) not null,
+	lastName varchar2(20) not null,
+	address varchar2(100) not null,
+	creditCard number(11) not null,
+  CONSTRAINT customer_id PRIMARY KEY (creditCard)
+)
+
 CREATE OR REPLACE PACKAGE creditCardGenerator AS
-   PROCEDURE startGenerating;
+   FUNCTION startGenerating RETURN integer;
    PROCEDURE splitNumberIntoArray(n in number);
    PROCEDURE varcharToArray(text in varchar2);
    PROCEDURE iterateThroughArray;
    PROCEDURE generateCard;
-   
    randomNumber number;
-   type numbersArrayType is varray(16) of integer;
+   type numbersArrayType is varray(17) of integer;
    numbersArray numbersArrayType;
    creditCardNumber integer := 0;
 END creditCardGenerator;
 
 CREATE OR REPLACE PACKAGE BODY creditCardGenerator AS
 
-  procedure startGenerating is 
+  function startGenerating return integer is 
     begin 
     SELECT dbms_random.value(1000000000000000,9999999999999) num into randomNumber FROM dual;
       splitNumberIntoArray(round(randomNumber));
-      generateCard();
-            
+      generateCard;
+      return creditCardNumber;
     end startGenerating;
 
   procedure splitNumberIntoArray(n in number) is 
@@ -32,26 +39,21 @@ CREATE OR REPLACE PACKAGE BODY creditCardGenerator AS
     begin
       numbersArray := numbersArrayType(); 
       for i in 1 .. length(text) loop
-       DBMS_OUTPUT.PUT_LINE(substr(text,i,1));
        numbersArray.extend();
        numbersArray(i) := (to_number(substr(text,i,1)));
       end loop;
-      iterateThroughArray();
+      --iterateThroughArray();
     end;
     
-    
+    --helper procedure
     PROCEDURE iterateThroughArray is 
       begin
-       dbms_output.put_line('Array count' || numbersArray.count);
         FOR i in 1 .. numbersArray.count LOOP
         dbms_output.put_line(numbersArray(i));
        END LOOP;
       end;
     
-    
     PROCEDURE generateCard is 
-     type numbersArrayType is varray(17) of integer;
-     numbersArray numbersArrayType;
      sumInt integer;
      sumOfAll integer := 0;
      numInt integer;
@@ -62,7 +64,6 @@ CREATE OR REPLACE PACKAGE BODY creditCardGenerator AS
         numInt := numbersArray(i);
         sumInt := numInt + numInt; 
         if mod(i,2) = 1 or i = 0  then
-        dbms_output.put_line(sumInt);
           if sumInt >= 10 then
             sumOfAll := sumOfAll + (sumInt - 9);
             else 
@@ -80,58 +81,69 @@ CREATE OR REPLACE PACKAGE BODY creditCardGenerator AS
           creditCardString := creditCardString || to_char(numbersArray(i));
      END LOOP;
      creditCardNumber := to_number(creditCardString);
-    dbms_output.put_line('Card ' || creditCardNumber );
   end;
     
 end creditCardGenerator;
 
 
-declare 
-  type numbersArrayType is varray(17) of integer;
-   numbersArray numbersArrayType;
-   sumInt integer;
-   sumOfAll integer := 0;
-   numInt integer;
-   checkDigit integer;
-   creditCardString varchar(16);
-   creditCardNumber integer := 0;
-begin 
-  numbersArray := numbersArrayType(4,5,0,7,9,4,2,5,7,1,6,9,3,4,5);
-  for i in 1..numbersArray.count loop 
-    numInt := numbersArray(i);
-    sumInt := numInt + numInt; 
-    if mod(i,2) = 1 or i = 0  then
-    dbms_output.put_line(sumInt);
-      if sumInt >= 10 then
-        sumOfAll := sumOfAll + (sumInt - 9);
-        else 
-        sumOfAll :=  sumOfAll + sumInt; 
-      end if;
-    else 
-      sumOfAll := sumOfAll + numInt;
-    end if;
-  end loop;
-  checkDigit := mod(sumOfAll*9, 10);
-    numbersArray.extend();
-  numbersArray(16) := checkDigit; 
-  dbms_output.put_line('Printing');
-  dbms_output.put_line('SumOfAll ' || sumOfAll);
-  dbms_output.put_line('CheckDigit ' || checkDigit);
+
+
+CREATE OR REPLACE PROCEDURE data_generator (limit_number IN PLS_INTEGER DEFAULT 100) IS
+    CURSOR tableCursor IS
+    select first_name, last_name
+    from fullname_table;
+    
+    first_name fullname_table.first_name%type;
+    last_name fullname_table.last_name%type;
+    creditCard integer := 0;
+    fetchedRecords integer := 0;
+BEGIN   
+   OPEN tableCursor;
   
-  creditCardString := '';
-   FOR i in 1 .. numbersArray.count LOOP
-        creditCardString := creditCardString || to_char(numbersArray(i));
-   END LOOP;
-   creditCardNumber := to_number(creditCardString);
-  dbms_output.put_line('Card ' || creditCardNumber );
-end; 
-/
+  LOOP
+    FETCH tableCursor INTO first_name, last_name; 
+    fetchedRecords := fetchedRecords + 1;
+    EXIT WHEN tableCursor%NOTFOUND;
+    EXIT WHEN fetchedRecords = limit_number;
+    creditCard := creditCardGenerator.startGenerating;
+    dbms_output.put_line('Jmeno: ' || last_name || ' ' || first_name || ' Card ' || creditCard);
+  END LOOP;
+
+  -- kurzor je vhodne i zavrit
+  CLOSE tableCursor;
+END data_generator;
+
+
+
+
+
+
+
+
+declare
+
+begin
+data_generator(10);
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 declare 
-
+ creditCard integer := 0;
 begin 
-  creditCardGenerator.startGenerating;
+ creditCard := creditCardGenerator.startGenerating;
+ dbms_output.put_line('Card ' || creditCard);
 end;
 /
-
